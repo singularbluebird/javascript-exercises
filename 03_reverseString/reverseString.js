270c508754368b90671af617878f9a0e113240e3
Samuel <samysarria.ss@gmail.com>
2022-07-08 00:11:31 -0300

    Adds function that reverses strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    let reversed = "";
+
+    for (let i = string.length; i >= 0; i--) {
+        reversed += string.charAt(i);
+    }
+
+    return reversed;
 
 };
 
