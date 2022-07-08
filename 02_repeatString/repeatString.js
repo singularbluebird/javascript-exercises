40584750bbc8f248df2399c8caae3ef980509cee
Samuel <samysarria.ss@gmail.com>
2022-07-07 23:59:33 -0300

    Writes function that repeats string many times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    //get an empty string to put all the repetitions in there.
+    let word = "";
 
+    for (let i = 0; i < num; i++) {
+        word += string;
+    }
+
+    return word;
 };
 
 // Do not edit below this line
