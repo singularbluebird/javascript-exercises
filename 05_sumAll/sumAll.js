cf9a126e752a228f77b5682b14bde331f8f5bab8
Samuel <samysarria.ss@gmail.com>
2022-07-09 15:21:59 -0300

    Adds function body to achieve desired result

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,11 @@
-const sumAll = function() {
+const sumAll = function(k, n) {
+
+    if (typeof (n+k) !== 'number') return 'ERROR';
+    else if (n < 0 || k < 0) return 'ERROR';
+
+    let answer = ((n + k) * (Math.abs(n - k) + 1) / 2);
+
+    return answer;
 
 };
 
