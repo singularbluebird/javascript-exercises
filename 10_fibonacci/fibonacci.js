89466620e881d61a6c56a971a38c9036ab0c2a3a
Samuel <samysarria.ss@gmail.com>
2022-07-22 19:03:44 -0300

    Adds function that returns fibonacci number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    if (typeof num === "string") num = parseInt(num);
 
+    let fib1 = 1
+    let fib2 = 1;
+    let bucket = 0;
+    let list = [];
+    list.push(fib1, fib2)
+
+    for (let i = 0; i < num; i++) {
+        bucket = fib2;
+        fib2 += fib1;
+        fib1 = bucket;
+        list.push(fib2);
+    }
+
+    return list[num - 1];
 };
 
 // Do not edit below this line
