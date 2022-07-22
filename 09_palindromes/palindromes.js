658a21c3fdaa24db4022cf12583ff05b75c425c3
Samuel <samysarria.ss@gmail.com>
2022-07-22 18:49:19 -0300

    Adds function to detect palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase();
+    word = word.split("");
+    const punct = word.filter(letter => (letter !== "." && 
+    letter !== "!" && letter !== " " && letter !== "," && letter !== "?"));
+    word = punct.join("");
+    return (word.split("").reverse().join("") === word);
 };
 
 // Do not edit below this line
