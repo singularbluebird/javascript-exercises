95dd2961541a3fd9afea511e2957f0cf97ecfaee
Samuel <samysarria.ss@gmail.com>
2022-07-23 00:14:51 -0300

    Adds function that returns the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
 
+    const stillAlive = obj.find(alive => alive.yearOfDeath === undefined);
+
+    let thisYear = new Date().getFullYear();
+
+    if (stillAlive) stillAlive['yearOfDeath'] = thisYear;
+    
+    const oldest = obj.sort((a, b) => {
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+
+        return lastGuy > nextGuy ? -1 : 1;
+    });
+
+    return oldest[0];
 };
 
 // Do not edit below this line
